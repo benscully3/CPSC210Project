645a22cd05d33566453b1aa7e85e8709854aef27
scull <ben13.students.cs.ubc.ca>
2022-10-09 17:17:17 -0700

    Added exception specifications

--- src/main/exceptions/BadCommand.java
+++ src/main/exceptions/BadCommand.java
@@ -1,4 +1,5 @@
 package exceptions;
 
+// Exception for when the wrong command is inputted
 public class BadCommand extends Exception {
 }
--- src/main/exceptions/NameAlreadyUsed.java
+++ src/main/exceptions/NameAlreadyUsed.java
@@ -1,4 +1,5 @@
 package exceptions;
 
+// Exception for when a name has already been used
 public class NameAlreadyUsed extends Exception {
 }
