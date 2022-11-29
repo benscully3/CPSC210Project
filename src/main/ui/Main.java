0aad5c6b90561db267d2a7da0bbd7362a5c74a09
scull <ben13.students.cs.ubc.ca>
2022-11-29 11:26:36 -0800

    Added example eventLog output to README.md

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,8 +3,8 @@ package ui;
 public class Main {
     public static void main(String[] args) {
 
-
-        //new GalaxyBuilderApp();
+        // Uncomment following line for console UI
+//        new GalaxyBuilderApp();
 
         // Uncomment following line for GUI
         new GalaxyBuilderGUI();
